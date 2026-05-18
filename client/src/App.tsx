2f3652b05a5890c924bdc4bc73ce9633f0144423
sammern <samreactdev@gmail.com>
2026-05-18 13:01:36 +0300

    updated routes index file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./routes/ProtectedRoute";
+import { ProtectedRoute } from "./routes";
 import { Login, Signup, Home, Profile } from "./pages";
 import "./App.scss";
 
